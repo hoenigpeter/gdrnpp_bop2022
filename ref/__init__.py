f67585fd630ccd1cc16b08e11c87eca0887e0b11
Peter Hoenig <hpfreak1004@gmail.com>
2025-05-14 15:55:06 +0200

    fix import of previously removed dataset configs

--- ref/__init__.py
+++ ref/__init__.py
@@ -1,1 +1,1 @@
-from . import keypose_transparent, keypose_random_texture, ycb_ichores, lmo_full, ycbv, hb, hb_bop19, tudl, tless, tless_random_texture, tless_3r, tless_7r, tless_5r, icbin, itodd, itodd_random_texture, lm_full, lmo_full
+from . import  ycb_ichores, lmo_full, ycbv, hb, hb_bop19, tudl, tless, icbin, itodd, lm_full
